1d01cf5d0a76397585c658e49942802d0ecf2fd7
RitanshRajput <it2042@global.org.in>
2023-12-06 20:12:16 +0530

    day21 MUI Social App

---
+++ 17.MUI_Social_App/src/components/Navbar.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import img from "../assets/img.png";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          DamnDude
+        </Typography>
+        <AcUnitIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={1} color="error">
+            <NotificationsIcon />
+          </Badge>
+          <Avatar
+            sx={{ width: "35px", height: "35px" }}
+            src={img}
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar sx={{ width: "35px", height: "35px" }} src={img} />
+          <Typography variant="span">Ritz</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl} 
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
